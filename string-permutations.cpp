bd5ac806e38385ddc0911f5978af85db00859338
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-04-01 00:30:18 -0700

    added function to generate unique permutations

--- string-permutations.cpp
+++ string-permutations.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+//the below function is to check for permutations without considering duplicates
 void permutation(string s, int i, int n) {
     
 	if (i == n-1) {
@@ -20,6 +20,28 @@ void permutation(string s, int i, int n) {
 	}
 
 }
+
+//the below implementation is to check for unique string permutations
+//ex aab = aab aba baa
+void permutationsUnique(string s, int i, int n) {
+    
+	if (i == n-1) {
+		cout << s << endl;
+        return;
+	}
+	else {
+		char temp;
+		for (int j =i; j <n; j++) {
+            // cout<<"swapping "<<s[i]<<" With "<<s[j]<<endl;
+            	if(s[i] != s[j] || i==j ){
+            	    swap(s[i],s[j]);
+            	    permutationsUnique(s, i + 1, n);
+            	    swap(s[i],s[j]);
+            		}
+		}
+	}
+}
+
 int main() {
 	string s = "abc";
 	permutation(s, 0, s.length());
