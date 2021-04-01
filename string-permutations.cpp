ad68a79d885dba14911734801d1e0e077d722326
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-03-31 23:37:26 -0700

    Update string-permutations.cpp

--- string-permutations.cpp
+++ string-permutations.cpp
@@ -4,15 +4,16 @@ using namespace std;
 void permutation(string s, int i, int n) {
     
 	if (i == n-1) {
+		//if we can make no more permutations then it means that we need to stop the current execution and print the current permutation.
 		cout << s << endl;
-        return 
+        	return; 
 	}
 	else {
 		char temp;
 		for (int j =i; j < n; j++) {
-			swap(s[i],s[j]);
-			permutation(s, i + 1, n);
-            swap(s[i],s[j]);
+			swap(s[i],s[j]);//first swaps the number 
+			permutation(s, i + 1, n);//then checks for new permutation
+           		swap(s[i],s[j]);//then backtracks
             
 		}
         
