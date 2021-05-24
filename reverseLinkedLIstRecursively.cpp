ee1dc61d72ec518b3672eaaf94a8d7ab932b0cca
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-05-24 15:04:10 +0530

    Update reverseLinkedLIstRecursively.cpp

--- reverseLinkedLIstRecursively.cpp
+++ reverseLinkedLIstRecursively.cpp
@@ -8,6 +8,10 @@ void reverseRecursion(struct Node *ptr)
     }
     reverseRecursion(ptr->next);
     struct Node * q = ptr->next;
-    q->next = ptr;
+    q->next = ptr;// instead of the above statements use: ptr->next->next = ptr;
     ptr->next = NULL;
+    // 10 > 20 > 30 > 50(ptr) > 60(ptr->next)
+    //  ptr->next->next = ptr; makes 
+    // 10 > 20 > 30 > 50(ptr) < 60(ptr->next)
+    //                 v(null)
 }
